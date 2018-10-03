14830848a6ad32a990fa009fe81441b0b2e4c9e5
mama frantz <hyunfrantz@gmail.com>
2018-10-03 02:25:49 -0400

    remove default react page, add background image, new header

--- src/App.js
+++ src/App.js
@@ -7,19 +7,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            Halloween Movie Collection
         </header>
+        <movie className= "movieListRow">
+          <ul>
+            <li>Hocus Pocus</li>
+          </ul>
+          <ul>
+            <li>Hocus Pocus</li>
+          </ul>
+        </movie>
       </div>
     );
   }
