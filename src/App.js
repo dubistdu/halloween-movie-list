5bf15863c3387b8715af32c3aa3a98f04a2973f1
mama frantz <hyunfrantz@gmail.com>
2018-10-04 11:03:24 -0400

    some style change and add data jason file to src

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './header.js';
 import Movies from './movies.js';
+import './movie-data.json';
 
 class App extends Component {
   render() {
--- src/movie.js
+++ src/movie.js
@@ -3,16 +3,14 @@ import React, { Component } from 'react';
 class Movie extends Component {
   render() {
     return (
-      <div>
+      <div className= "movie">
         <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg"/>
         <ul>
-          <li>original_title: "Indiana Jones and the Last Crusade"</li>
-          <li>vote_average: 7.7,</li>
-          <li>title: "Indiana Jones and the Last Crusade"</li>
-          <li>original_language: "en",</li>
-          <li>genre_ids: [12, 28]</li>
-          <li>overview:
-            When Dr. Henry Jones Sr. suddenly goes missing while pursuing the Holy Grail, eminent archaeologist Indiana must team up with Marcus Brody, Sallah and Elsa Schneider to follow in his father's footsteps and stop the Nazis from recovering the power of eternal life.",
+          <li><strong>Title:</strong> "Indiana Jones and the Last Crusade"</li>
+          <li><strong>Rating: 7.7,</strong></li>
+          <li><strong>Original Language:</strong> "en"</li>
+          <li><strong>Overview: </strong>
+            When Dr. Henry Jones Sr. suddenly goes missing while pursuing the Holy Grail, eminent archaeologist Indiana must team up with Marcus Brody, Sallah and Elsa Schneider to follow in his father's footsteps and stop the Nazis from recovering the power of eternal life."
           </li>
         </ul>
       </div>
