4b92ec5ac7da26a9bf372156867fa4c7f94e8f57
mama frantz <hyunfrantz@gmail.com>
2018-10-22 15:05:07 -0400

    movies api fetch

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import Movie from './movie.js';
-import movies from './movie-data.json';
+// import movies from './movie-data.json';
 
 class MovieList extends Component {
+  constructor() {
+    super();
+    this.state = {movies: []}
+  }
+
+  // When this component appears on the HTML
+  componentDidMount() {
+    //  get the value from the input field and request that data be fetched by
+    // after fetching make json obj
+    const API_KEY = process.env.REACT_APP_MOVIE_DB_API_KEY;
+
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?primary_release_year=1991&sort_by=popularity.desc&api_key=`${API_Key}`'
+    )
+    .then(response => response.json())
+    .then(({results: movies}) => this.setState({movies}))
+    }
+
+
   render() {
+    let movies = this.state.movies
     return (
       <div className= "movieList">
-        {movies.results
-            .sort((a, b) => {
+        {movies.sort((a, b) => {
               if (a.release_date < b.release_date) {
                 return -1;
               } else if (a.release_date > b.release_date) {
@@ -28,10 +47,9 @@ class MovieList extends Component {
                 />
               );
             })
-          }
+          };
         </div>
       );
     }
   }
-
 export default MovieList;
