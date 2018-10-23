8b6ea9fe0c0dd60e25f8acfff4ceba02c3e07f28
mama frantz <hyunfrantz@gmail.com>
2018-10-22 23:31:40 -0400

    load .env

--- src/movies.js
+++ src/movies.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Movie from './movie.js';
 // import movies from './movie-data.json';
 
+// to load .env file
+require("dotenv").config();
+
 class MovieList extends Component {
   constructor() {
     super();
@@ -15,7 +18,7 @@ class MovieList extends Component {
     const API_KEY = process.env.REACT_APP_MOVIE_DB_API_KEY;
 
     fetch(
-      'https://api.themoviedb.org/3/discover/movie?primary_release_year=1991&sort_by=popularity.desc&api_key=`${API_Key}`'
+      API_KEY
     )
     .then(response => response.json())
     .then(({results: movies}) => this.setState({movies}))
